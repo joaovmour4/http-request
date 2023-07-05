77b00772ef21924c5937d64f471c39a51a1d004a
joaovmour4 <joaov.mour@unifesspa.edu.br>
2023-07-05 12:28:06 -0300

    Alteração para Requisição HTTP

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,16 @@
-button = document.querySelector('#sendButton');
-nome = document.querySelector('#name');
-tel = document.querySelector('#phone');
-mail = document.querySelector('#mail');
+var button = document.querySelector('#sendButton');
+var inscricao = document.querySelector('#inscricao');
+var notInicial = document.querySelector('#notInicial');
+var notFinal = document.querySelector('#notFinal');
+
 
 button.onclick = function(){
-    alert('Nome: '+nome.value);
-    alert('Telefone: '+tel.value);
-    alert('E-Mail: '+mail.value);  
+    let request = new XMLHttpRequest();
+    for(var i=notInicial.value; i<=notInicial.value; i++){
+        var url = `http://nfe.maraba.pa.gov.br/eSiat/Valida_NFE_Emissao.aspx?InscricaoMunicipal=${inscricao.value}&NumeroNota=${i}&Origem=RELACAO`;
+        request.open('GET', url);
+        request.setRequestHeader("Content-Type",
+                                    "text/plain;charset=UTF-8");
+        request.send(null);
+    }
 };
